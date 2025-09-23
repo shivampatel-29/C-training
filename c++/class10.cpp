9108e472f8879e3eeadf0b9619b863c67e40d6b8
shivampatel-29 <shivampatel2907@gmail.com>
2025-09-23 09:47:53 +0530

    class

---
+++ c++/class10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class myclass
+{
+    public:
+    int x;
+    private:
+    int y;
+};
+int main()
+{
+    myclass  myobj;
+    myobj.x=200;
+    myobj.y=3000;
+    return 0;
+}
