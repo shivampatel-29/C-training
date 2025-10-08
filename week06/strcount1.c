077e798536f8d6fcd1e47fdc45909aaadbb9193f
shivampatel-29 <shivampatel2907@gmail.com>
2025-10-08 15:24:50 +0530

    string

---
+++ week06/strcount1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[]="abca";
+    int freq[256]={0};
+    int ln=strlen(str);
+    for(int i=0;i<ln;i++)
+    {
+        freq[str[i]]++;
+    }
+    for(int i=0;i<256;i++)
+    {
+        if(freq[i]==1)
+        {
+            printf("  %c : %d ",i,freq[i]);
+            break;
+        }
+    }
+}
