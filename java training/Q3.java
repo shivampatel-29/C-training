075f53b3a101db8f2b15e21499c3ca72df37fcb0
shivampatel-29 <shivampatel2907@gmail.com>
2025-11-20 13:52:31 +0530

    ques

---
+++ java training/Q3.java
@@ -0,0 +1,17 @@
+interface A{
+      void login();
+}
+interface B{
+      void login();
+}
+class C implements A,B{
+    public void login(){
+        System.out.println("httt jaa saamne se");
+    }
+}
+public class Q3{
+    public static void main(String[] args){
+        C n1=new C();
+        n1.login();
+    }
+}
